1a1063d970c483f2fd78a6f1120d6ab3178a34e5
Mohamed Naiz <mohamed.naiz@outlook.com>
2025-01-10 17:57:32 +0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Mohamed Naiz on 10-1-25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
